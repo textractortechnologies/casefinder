58abda0c2a6948441daa848acbca33e845f7adc2
Eugene Vilensky <evilensky@gmail.com>
2015-02-21 15:35:46 -0600

    Postgresql dependency

--- manifests/site.pp
+++ manifests/site.pp
@@ -12,12 +12,27 @@ node casefinder {
 
   include apache
 
-  class { 'rvm::passenger::apache':
-    version            => '4.0.59',
-    ruby_version       => 'ruby-2.2',
-    mininstances       => '4',
-    maxinstancesperapp => '0',
-    maxpoolsize        => '30',
-  }
+  #  class { 'rvm::passenger::apache':
+  #    version            => '4.0.59',
+  #    ruby_version       => 'ruby-2.2',
+  #    mininstances       => '4',
+  #    maxinstancesperapp => '0',
+  #    maxpoolsize        => '30',
+  #  }
+
+  class { 'postgresql::globals':
+    manage_package_repo => true,
+    version             => '9.4',
+    postgis_version     => '2.1',
+  }->
+
+  class { 'postgresql::server':
+  ip_mask_deny_postgres_user => '0.0.0.0/32',
+  ip_mask_allow_all_users    => '0.0.0.0/0',
+  listen_addresses           => '*',
+  ipv4acls                   => ['local all all md5'],
+  #development only
+  postgres_password          => 'ghewUkEck3',
+}
 
 }
