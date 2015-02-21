422a714ae8ad2c66a267c646179027dc10ea1786
Eugene Vilensky <evilensky@gmail.com>
2015-02-21 14:53:24 -0600

    Work in progress, does not yet build passenger successfully

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,2 +1,23 @@
 node casefinder {
+  include rvm
+
+  rvm_system_ruby {
+    'ruby-2.1':
+      ensure      => 'present',
+      default_use => false;
+    'ruby-2.2':
+      ensure      => 'present',
+      default_use => false;
+  }
+
+  include apache
+
+  class { 'rvm::passenger::apache':
+    version            => '4.0.59',
+    ruby_version       => 'ruby-2.2',
+    mininstances       => '4',
+    maxinstancesperapp => '0',
+    maxpoolsize        => '30',
+  }
+
 }
