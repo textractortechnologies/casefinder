862d4155c169b97d3c9756710d01601debc5cdd8
Eugene Vilensky <evilensky@gmail.com>
2015-02-25 13:52:55 -0600

    pg_hba rules and database creation

--- manifests/site.pp
+++ manifests/site.pp
@@ -10,29 +10,45 @@ node casefinder {
       default_use => false;
   }
 
-  include apache
-
-  #  class { 'rvm::passenger::apache':
-  #    version            => '4.0.59',
-  #    ruby_version       => 'ruby-2.2',
-  #    mininstances       => '4',
-  #    maxinstancesperapp => '0',
-  #    maxpoolsize        => '30',
-  #  }
-
   class { 'postgresql::globals':
     manage_package_repo => true,
     version             => '9.4',
     postgis_version     => '2.1',
-  }->
+    }->
 
-  class { 'postgresql::server':
-  ip_mask_deny_postgres_user => '0.0.0.0/32',
-  ip_mask_allow_all_users    => '0.0.0.0/0',
-  listen_addresses           => '*',
-  ipv4acls                   => ['local all all md5'],
-  #development only
-  postgres_password          => 'ghewUkEck3',
-}
+    class { 'postgresql::server':
+      ip_mask_deny_postgres_user => '0.0.0.0/32',
+      ip_mask_allow_all_users    => '0.0.0.0/0',
+      listen_addresses           => '*',
+      ipv4acls                   => ['local all all md5'],
+      #development only
+      postgres_password          => 'ghewUkEck3',
+    } ->
+
+    class { 'postgresql_backup':
+      backup_path => '/var/lib/pgsql/9.4/backups',
+    }
+
+    postgresql::server::pg_hba_rule { 'allow localhost access by casefinder':
+      description => "allow localhost access by casefinder",
+      type => 'host',
+      database => 'casefinder',
+      user => 'casefinder',
+      address => '127.0.0.1',
+      auth_method => 'md5',
+    }
+
+    postgresql::server::db { 'casefinder':
+      user     => 'casefinder_user',
+      password => 'DabcosyupKoacDytmilejRumHypHeOp6',
+    }
+
+
+
+
+
+
+
+
 
 }
