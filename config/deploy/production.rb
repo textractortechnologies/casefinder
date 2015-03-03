e6ed629fc37e2ffa4539742bee1f55d0341a36ca
MICHAEL GURLEY <michaeljamesgurley@gmail.com>
2015-03-03 17:14:39 -0600

    Change name of server for production deployment.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -17,7 +17,7 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@linux.local}
+role :app, %w{deploy@casefinder.local}
 # role :web, %w{deploy@example.com}
 # role :db,  %w{deploy@example.com}
 
@@ -28,7 +28,7 @@ role :app, %w{deploy@linux.local}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'deploy@linux.local', user: 'deploy', roles: %w{web app}#, my_property: :my_value
+server 'deploy@casefinder.local', user: 'deploy', roles: %w{web app}#, my_property: :my_value
 
 
 # Custom SSH Options
