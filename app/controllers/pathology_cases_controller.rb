7c8191f7eec98e83cfd5b672fd5bb250282460cf
MICHAEL GURLEY <michaeljamesgurley@gmail.com>
2015-03-24 03:50:05 -0500

    close #26 Filters for listing pathology cases.

--- app/controllers/pathology_cases_controller.rb
+++ app/controllers/pathology_cases_controller.rb
@@ -1,7 +1,9 @@
 class PathologyCasesController < ApplicationController
   def index
     params[:filter] ||= Abstractor::Enum::ABSTRACTION_STATUS_NEEDS_REVIEW
-    @pathology_cases = PathologyCase.search_across_fields(params[:search]).by_abstractor_abstraction_status(params[:filter]).by_encounter_date(params[:date_from], params[:date_to]).order('encounter_date ASC').paginate(:per_page => 10, :page => params[:page])
+    params[:suggestion_filter] ||= Abstractor::Enum::ABSTRACTION_SUGGESTION_TYPE_NOT_UNKNOWN
+    abstractor_abstraction_schema_cancer_histology = PathologyCase.abstractor_abstraction_schemas.detect { |abstractor_abstraction_schema| abstractor_abstraction_schema.display_name = 'Cancer Histology' }
+    @pathology_cases = PathologyCase.search_across_fields(params[:search]).by_abstractor_abstraction_status(params[:filter]).by_abstractor_suggestion_type(params[:suggestion_filter], abstractor_abstraction_schemas: abstractor_abstraction_schema_cancer_histology).by_encounter_date(params[:date_from], params[:date_to]).order('encounter_date ASC').paginate(:per_page => 10, :page => params[:page])
   end
 
   def edit
