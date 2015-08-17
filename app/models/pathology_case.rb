cc482184b042b85c4595f706f800a3e30d7a2e5b
MICHAEL GURLEY <michaeljamesgurley@gmail.com>
2015-08-17 07:11:12 -0500

    references #122 Move the placement on the queue to after all the pathology cases are saved.

--- app/models/pathology_case.rb
+++ app/models/pathology_case.rb
@@ -1,5 +1,6 @@
 require 'roo'
 require 'csv'
+require "benchmark"
 class PathologyCase < ActiveRecord::Base
   include Abstractor::Abstractable
 
@@ -68,39 +69,54 @@ class PathologyCase < ActiveRecord::Base
     spreadsheet = open_spreadsheet(file)
     header = spreadsheet.row(1)
     pathology_case = nil
+    saved_pathology_cases = []
     note = ''
     (2..spreadsheet.last_row).each do |i|
       if spreadsheet.row(i).compact.size > 3
-        save_pathology_case(pathology_case, note)
-        row = Hash[[header, spreadsheet.row(i)].transpose]
-        note = ''
-        accession_num = spreadsheet.row(i)[5].is_a?(Float) ? spreadsheet.row(i)[5].to_i.to_s : spreadsheet.row(i)[5]
-        pathology_case = PathologyCase.where(accession_number: accession_num).first_or_initialize
-        pathology_case.collection_date       = DateTime.parse(spreadsheet.row(i)[13].to_s.strip).to_date
-        patient_last_name, patient_first_name = spreadsheet.row(i)[1].split(',')
-        pathology_case.patient_last_name          = patient_last_name.strip
-        pathology_case.patient_first_name          = patient_first_name.strip
-        mrn = spreadsheet.row(i)[3].is_a?(Float) ? spreadsheet.row(i)[3].to_i.to_s : spreadsheet.row(i)[3]
-        pathology_case.mrn                  = mrn
-        pathology_case.ssn                  = spreadsheet.row(i)[15]
-        pathology_case.birth_date           = DateTime.parse(spreadsheet.row(i)[7].to_s.strip).to_date unless spreadsheet.row(i)[7].blank?
-        pathology_case.street1              = spreadsheet.row(i)[17]
-        pathology_case.street2              = spreadsheet.row(i)[19]
-        pathology_case.city                 = spreadsheet.row(i)[21]
-        pathology_case.state                = spreadsheet.row(i)[23]
-        zip_code = spreadsheet.row(i)[25].is_a?(Float) ? spreadsheet.row(i)[25].to_i.to_s : spreadsheet.row(i)[25]
-        pathology_case.zip_code             = zip_code
-        pathology_case.country              = spreadsheet.row(i)[27]
-        pathology_case.home_phone           = spreadsheet.row(i)[29]
-        pathology_case.sex                  = spreadsheet.row(i)[9]
-        pathology_case.race                 = spreadsheet.row(i)[11]
-        pathology_case.attending            = spreadsheet.row(i)[31]
-        pathology_case.surgeon              = spreadsheet.row(i)[33].blank? ? spreadsheet.row(i)[31] :  spreadsheet.row(i)[31]
+        time = Benchmark.realtime do
+          save_pathology_case(pathology_case, note)
+          if pathology_case
+            saved_pathology_cases << pathology_case
+          end
+          note = ''
+          accession_num = spreadsheet.row(i)[5].is_a?(Float) ? spreadsheet.row(i)[5].to_i.to_s : spreadsheet.row(i)[5]
+          pathology_case = PathologyCase.where(accession_number: accession_num).first_or_initialize
+          pathology_case.collection_date       = DateTime.parse(spreadsheet.row(i)[13].to_s.strip).to_date
+          patient_last_name, patient_first_name = spreadsheet.row(i)[1].split(',')
+          pathology_case.patient_last_name          = patient_last_name.strip
+          pathology_case.patient_first_name          = patient_first_name.strip
+          mrn = spreadsheet.row(i)[3].is_a?(Float) ? spreadsheet.row(i)[3].to_i.to_s : spreadsheet.row(i)[3]
+          pathology_case.mrn                  = mrn
+          pathology_case.ssn                  = spreadsheet.row(i)[15]
+          pathology_case.birth_date           = DateTime.parse(spreadsheet.row(i)[7].to_s.strip).to_date unless spreadsheet.row(i)[7].blank?
+          pathology_case.street1              = spreadsheet.row(i)[17]
+          pathology_case.street2              = spreadsheet.row(i)[19]
+          pathology_case.city                 = spreadsheet.row(i)[21]
+          pathology_case.state                = spreadsheet.row(i)[23]
+          zip_code = spreadsheet.row(i)[25].is_a?(Float) ? spreadsheet.row(i)[25].to_i.to_s : spreadsheet.row(i)[25]
+          pathology_case.zip_code             = zip_code
+          pathology_case.country              = spreadsheet.row(i)[27]
+          pathology_case.home_phone           = spreadsheet.row(i)[29]
+          pathology_case.sex                  = spreadsheet.row(i)[9]
+          pathology_case.race                 = spreadsheet.row(i)[11]
+          pathology_case.attending            = spreadsheet.row(i)[31]
+          pathology_case.surgeon              = spreadsheet.row(i)[33].blank? ? spreadsheet.row(i)[31] :  spreadsheet.row(i)[31]
+        end
+        Rails.logger.info("Patient row: Little my says how long time elapsed #{time*1000} milliseconds)")
       else
-        note += spreadsheet.row(i).compact.join(' ') +  "\r\n"
+        time = Benchmark.realtime do
+          note += spreadsheet.row(i).compact.join(' ') +  "\r\n"
+        end
+        Rails.logger.info("Note row: Little my says how long time elapsed #{time*1000} milliseconds)")
       end
     end
     save_pathology_case(pathology_case, note)
+    if pathology_case
+      saved_pathology_cases << pathology_case
+    end
+    saved_pathology_cases.each do |saved_pathology_case|
+      Delayed::Job.enqueue ProcessPathologyCaseJob.new(saved_pathology_case.id)
+    end
   end
 
   def self.save_pathology_case(pathology_case, note)
@@ -108,8 +124,8 @@ class PathologyCase < ActiveRecord::Base
       note.gsub!('_x000D_', '')
       pathology_case.note = note
       pathology_case.save!
-      Delayed::Job.enqueue ProcessPathologyCaseJob.new(pathology_case.id)
     end
+    pathology_case
   end
 
   def self.open_spreadsheet(file)
