2124c8e6990dffc20eab02d24a5241ee3bd2164b
MICHAEL GURLEY <michaeljamesgurley@gmail.com>
2015-08-24 19:48:00 -0500

    references #47 Set configuration of SMTP server correctly in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -141,7 +141,7 @@ Rails.application.configure do
     }
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { host: 'smtp.enhnet.org', port: 25 }
+  config.action_mailer.smtp_settings = { address: 'smtp.enhnet.org', port: 25 }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 end
