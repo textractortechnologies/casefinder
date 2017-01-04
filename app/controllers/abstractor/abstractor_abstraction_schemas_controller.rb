abec67fea54e96e0777f55f62a78c94b7581953f
MICHAEL GURLEY <michaeljamesgurley@gmail.com>
2017-01-04 07:43:23 -0500

    closes #55 Implement outbound token authentication. closes #52 Implement inbound token API authentication.

--- app/controllers/abstractor/abstractor_abstraction_schemas_controller.rb
+++ app/controllers/abstractor/abstractor_abstraction_schemas_controller.rb
@@ -5,6 +5,7 @@ module Abstractor
   class AbstractorAbstractionSchemasController < ApplicationController
     include Abstractor::Methods::Controllers::AbstractorAbstractionSchemasController
     include Abstractor::AbstractorAbstractionSchemasControllerCustomMethods
-    # acts_as_token_authentication_handler_for User, only: [:show]
+    acts_as_token_authentication_handler_for User, only: [:show]
+    protect_from_forgery except: [:show]
   end
 end
--- app/controllers/abstractor/abstractor_suggestions_controller.rb
+++ app/controllers/abstractor/abstractor_suggestions_controller.rb
@@ -5,7 +5,7 @@ module Abstractor
   class AbstractorSuggestionsController < ApplicationController
     include Abstractor::Methods::Controllers::AbstractorSuggestionsController
     include Abstractor::AbstractorSuggestionsControllerCustomMethods
-    # acts_as_token_authentication_handler_for User, only: [:update]
+    acts_as_token_authentication_handler_for User, only: [:create]
     protect_from_forgery except: [:create]
   end
 end
