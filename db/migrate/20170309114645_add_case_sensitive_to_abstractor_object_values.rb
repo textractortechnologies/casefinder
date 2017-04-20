78d28b75910722b69090e15f41b3ee93c40dc18c
MICHAEL GURLEY <michaeljamesgurley@gmail.com>
2017-04-20 01:47:44 -0500

    references #211 Remove duplicate migrations.

--- db/migrate/20170309114645_add_case_sensitive_to_abstractor_object_values.rb
+++
@@ -1,5 +0,0 @@
-class AddCaseSensitiveToAbstractorObjectValues < ActiveRecord::Migration
-  def change
-    add_column :abstractor_object_values, :case_sensitive, :boolean, default: false
-  end
-end
--- db/migrate/20170309114656_add_case_sensitive_to_abstractor_object_value_variants.rb
+++
@@ -1,5 +0,0 @@
-class AddCaseSensitiveToAbstractorObjectValueVariants < ActiveRecord::Migration
-  def change
-    add_column :abstractor_object_value_variants, :case_sensitive, :boolean, default: false
-  end
-end
